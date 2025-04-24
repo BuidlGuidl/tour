feda0e6a7bdb1b2227bfa9c125c5bcf48a1d49b9
Andrea Albiac <andrealbiac@gmail.com>
2025-04-24 10:27:54 +0200

    Re-styled markers and tooltip

--- packages/nextjs/components/Markers.tsx
+++ packages/nextjs/components/Markers.tsx
@@ -10,23 +10,22 @@ type MarkerProps = {
 };
 
 const Marker: React.FC<MarkerProps> = ({ left, top, label, color = "green" }) => {
-  const gradientClasses = {
-    green: "from-green-500 to-emerald-500 shadow-green-300",
-    yellow: "from-yellow-400 to-amber-500 shadow-yellow-300",
+  const colorClasses = {
+    green: "bg-[#90fc90]",
+    yellow: "bg-[#fff97f]",
   };
 
   return (
     <div className="absolute group cursor-pointer" style={{ left, top }}>
       {/* Fancy pin shape with shadow and glow */}
       <div
-        className={`w-4 h-4 bg-gradient-to-br ${gradientClasses[color]} rounded-full border-[3px] border-white shadow-md animate-pulse`}
+        className={`w-4 h-4 ${colorClasses[color]} rounded-full border-[1.3px] border-[#886e4d] shadow-sm shadow-[#a2845ed6]`}
       />
 
       {/* Tooltip on hover */}
       <div className="absolute left-1/2 -translate-x-1/2 bottom-full mb-2 whitespace-nowrap z-20 hidden group-hover:block">
-        <div className="bg-white px-3 py-1 rounded-full font-ppwriter text-black shadow-xl relative transition-all duration-200">
+        <div className="bg-[#faf1e5] px-3 py-1 rounded-full font-ppwriter text-[#614c31] shadow-md border-[1.3px] border-[#886e4d] shadow-[#886f4eb7] relative transition-all duration-200 after:content-[''] after:absolute after:w-2 after:h-2 after:bg-[#faf1e5] after:rotate-45 after:-bottom-1 after:left-1/2 after:-translate-x-1/2 after:border-[1.3px] after:border-[#886e4d] after:border-t-0 after:border-l-0">
           {label}
-          <div className="absolute w-2 h-2 bg-white rotate-45 -bottom-1 left-1/2 -translate-x-1/2" />
         </div>
       </div>
     </div>
